d557074844d2a12c772c17817f1431756eb37bf6
oliverhynes21-code <oliverhynes21@gmail.com>
2026-01-06 20:18:34 +0000

    Updated the python code with more validation and specific categories, adding the framework for future features such as monthly summaries or simple data visualisation.

--- expenseTracker.py
+++ expenseTracker.py
@@ -3,7 +3,10 @@
 import csv, os
 from datetime import date
 
-while True:
+userExit = False
+validCategories = {"housing", "transport", "food", "bills", "communication", "leisure", "healthcare", "clothing", "misc"}
+
+while not userExit:
 
     currentDate = date.today().isoformat()
     fileExists = os.path.isfile("data/expenses.csv")
@@ -12,29 +15,45 @@ while True:
     userAction = int(input("1. Add Expense\n2. Read Expenses\n3. Quit\nSelect Action (1-3): "))
 
     if userAction == 1:
-        expenseCategory = input("Enter expense category: ")
-        expensePrice = float(input("Enter expense price: "))
+        expenseCategory = input("Enter category (Housing/Transport/Food/Bills/Communication/Leisure/Healthcare/Clothing/Misc): ").strip().lower()
 
-        #Opened file in context manager, doesn't need to be closed
-        with open("data/expenses.csv", "a", newline='') as expenseFile:
-            writer = csv.writer(expenseFile)
+        if expenseCategory not in validCategories:
+            print("Invalid category.")
+        #Adding validation of categories as framework for future features (spending totals, potential GUI with tkinter, etc.)
 
-            if not fileExists:
-                #Adding a header to the file if it is empty
-                writer.writerow(["Date", "Category", "Price"])
+        else:
+            expenseItem = input("Enter item: ")
 
-            writer.writerow([currentDate, expenseCategory, expensePrice])
+            #Validation coding for the price of the expense, repeatedly checks user input and asks for a numeric value until a satisfactory value is given
+            priceAccepted = False
+            while not priceAccepted:
+                try:
+                    expensePrice = float(input("Enter price: "))
+                    priceAccepted = True
+                except ValueError:
+                    print("Please enter a numeric value.")
+
+            #Opened file in context manager, doesn't need to be closed
+            with open("data/expenses.csv", "a", newline='') as expenseFile:
+                writer = csv.writer(expenseFile)
+
+                if not fileExists:
+                    #Adding a header to the file if it is empty
+                    writer.writerow(["Date", "Category", "Item", "Price"])
+
+                writer.writerow([currentDate, expenseCategory,expenseItem, expensePrice])
 
     elif userAction == 2:
         if not fileExists:
             print("No expenses found.")
+
         else:
             #Opened file in read mode
             with open("data/expenses.csv", "r") as expenseFile:
                 reader = csv.reader(expenseFile)
                 for row in reader:
                     #Removes the unnecessary characters from the row and adds a comma inbetween row elements
-                    print(*row, sep=",")
+                    print(row)
 
     elif userAction == 3:
-        break
+        userExit = True
